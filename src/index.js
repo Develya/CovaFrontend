580fce852da8f6c288c51c2d61f69574a76e7910
Francis Beaulieu-Ménard <2072827@colval.qc.ca>
2024-04-22 08:55:13 -0400

    Fix the fuck out of ArRianNe

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
-import App from './App';
+import Register from './Register';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 
@@ -12,7 +12,7 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Register />
     </Provider>
   </React.StrictMode>
 );
