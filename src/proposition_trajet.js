d3b9a03bc545d4ee8b408a4dc843aeaf038381ea
Francis Beaulieu-Ménard <2072827@colval.qc.ca>
2024-05-07 13:39:04 -0400

    Je pensais être à la retraite mais je voulais dire A VA RIEN FAIRE LA PETITE VEL TU L'AS COMPRENDS TU CELLE LÀ!

--- src/proposition_trajet.js
+++ src/proposition_trajet.js
@@ -36,11 +36,11 @@ const PropositionTrajet = () => {
             const coords = point([parseFloat(data[0].lon), parseFloat(data[0].lat)]);
             return coords;
         } else {
-            //console.error("Aucune coordonnée trouvée pour l'adresse:", address);
+            console.error("Aucune coordonnée trouvée pour l'adresse:", address);
             return null;
         }
     } catch (error) {
-        //console.error('Erreur lors de la géocodage de l\'adresse:', error);
+        console.error('Erreur lors de la géocodage de l\'adresse:', error);
         return null;
     }
 };
@@ -62,11 +62,9 @@ const PropositionTrajet = () => {
         // Vérifier si les coordonnées de départ et d'arrivée sont disponibles
         if (departureCoords && destinationCoords) {
             // Créer un cercle de 10 km autour du point de départ
-            //console.log("departureCoord sont en tabarnak: ", departureCoords)
             const departureCircle = circle(departureCoords.geometry.coordinates, 10, { units: 'kilometers' });
             
             departureCircle.geometry.coordinates[0].push(departureCoords.geometry.coordinates)
-            //console.log("hdoashdsoahd: ", departureCircle.geometry.coordinates[0])
             setDepartureCircle(departureCircle);
 
             // Créer un cercle de 10 km autour du point de destination
@@ -86,15 +84,12 @@ useEffect(() => {
     .then(response => response.json())
     .then(async (json) => {
         // Filtrer les trajets pour ne conserver que ceux avec des adresses valides
-        //console.log("tanarmal de vierge",json)
         const validJourneys = await Promise.all(json.map(async (journey) => {
             // Géocoder les adresses de départ et d'arrivée
             const departureCoords = await geocodeAddress(journey.trajet.departureAddress);
             const destinationCoords = await geocodeAddress(journey.trajet.destinationAddress);
-            //console.log("departureCoord", departureCoords)
-            //console.log("destinationCoord", destinationCoords)
             // Vérifier si les coordonnées ont été obtenues avec succès
-            if (departureCoords && destinationCoords) {
+             if (departureCoords && destinationCoords) {
                 // Ajouter les coordonnées géographiques au trajet
                 return {
                     ...journey,
@@ -106,16 +101,12 @@ useEffect(() => {
                 return null;
             }
         }));
-        //console.log("esti.... ", validJourneys)
         // Filtrer les trajets invalides (ceux avec des adresses non géocodées)
         const filteredJourneys = validJourneys.filter(journey => journey !== null);
-        //console.log("bon... ", filteredJourneys)
-        setalljourneys(filteredJourneys);
-        //console.log("all journeys: ",alljourneys)
+         setalljourneys(filteredJourneys);
 
-        filteredJourneys.map((journey) => {
+         filteredJourneys.map((journey) => {
             let destination = false
-            //console.log(journey)
             const cheh = departureCircle.geometry.coordinates
             cheh[0].map((departure) => {
                 if(journey.departureCoords.geometry.coordinates[0] == departure[0])
@@ -128,26 +119,25 @@ useEffect(() => {
                   if(journey.destinationCoords.geometry.coordinates[0] == des[0]){
                     if(journey.destinationCoords.geometry.coordinates[1] == des[1]){
                         setisInCircle((prevJourneys) => [...prevJourneys, journey]);
+                        
                     }
                   }
-                //console.log("isInCirlce: ", isInCircle)
                 })
             }
-            //console.log("the fuck")
         })
     })
-    .catch((error) => //console.log(error));
+    .catch((error) => console.log(error));
 }, [])
 
 const handleReservation = () => {
-    alert("reservation is done.")
+    alert("reservation is done. A va rien faire la petite vel")
 }
 
 
 return(
     <>
         {
-            data.map((item) => {
+            isInCircle.map((item) => {
                 return(
                     <div key={item.journeyID} >
                         <div className="address">
@@ -169,42 +159,6 @@ return(
         }
     </>
     /*<div>
-        <h1>Coordonnées de départ:</h1>
-        {departureCoords && (
-            <div>
-                <p>Latitude: {departureCoords.geometry.coordinates[1]}</p>
-                <p>Longitude: {departureCoords.geometry.coordinates[0]}</p>
-            </div>
-        )}
-
-        <h1>Coordonnées de destination:</h1>
-        {destinationCoords && (
-            <div>
-                <p>Latitude: {destinationCoords.geometry.coordinates[1]}</p>
-                <p>Longitude: {destinationCoords.geometry.coordinates[0]}</p>
-            </div>
-        )}
-        <h1>Distance entre les deux points:</h1>
-        {distanceBetweenPoints && (
-            <div>
-                <p>{distanceBetweenPoints} kilomètres</p>
-            </div>
-        )}
-        <h1>Cercles de 10 km:</h1>
-        {departureCircle && destinationCircle && (
-            <div>
-                <p>Cercle de départ: {JSON.stringify(departureCircle)}</p>
-                <p>Cercle de destination: {JSON.stringify(destinationCircle)}</p>
-            </div>
-        )}
-        <h1>All journeys</h1>
-        {//console.log("pu trop trop le choix: ",alljourneys)}
-        {alljourneys.map((journey, index) => (
-            <div key={index}>
-                <p>{JSON.stringify(journey.departureCoords.geometry.coordinates)} </p>
-                <>{JSON.stringify(journey.destinationCoords.geometry.coordinates)}</>
-            </div>
-        ))}
         {isInCircle && (
             <div>
                 <h1>Is in circle?? (Ce qui doit être affiché)</h1>
